c4df68d30323e4e59566b80ffeabee792aa978c5
Jon Church <me@jonchurch.com>
2025-01-06 16:35:59 -0500

    better centralize env config, update .env.example

--- config.js
+++ config.js
@@ -1,7 +1,11 @@
+require('dotenv').config()
+
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ?? 3000;
+const REPO_TYPES = process.env.REPO_TYPES ?? 'public'
 
 module.exports = {
   GITHUB_TOKEN,
-  PORT
+  PORT,
+  REPO_TYPES
 }
